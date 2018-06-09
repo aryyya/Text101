03dad6ae422c643136ee78e045b2975d8a06d07c
aryyya <arya@live.ca>
2018-06-08 20:51:56 -0700

    Add scriptable component for game state.

---
+++ Assets/State.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(0, 8)] [SerializeField] string storyText;
+}
---
+++ Assets/Text101.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Text101 : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+
+    void Start()
+    {
+        textComponent.text = "Hello, world!";
+    }
+
+    void Update()
+    {
+    }   
+}
